e69e8c990b93026d8793dea035a460bd011eee71
psych0d0g <lukas.wingerberg@springernature.com>
2018-09-07 15:55:04 +0200

    dependencies fix

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-release=$(curl --silent "https://api.github.com/repos/datacrow/core/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-wget https://github.com/datacrow/core/releases/download/${release}/datacrow-core.jar -O ./lib/datacrow-core.jar
+release_core=$(curl --silent "https://api.github.com/repos/datacrow/core/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+wget https://github.com/datacrow/core/releases/download/${release_core}/datacrow-core.jar -O ./lib/datacrow-core.jar
+release_server=$(curl --silent "https://api.github.com/repos/datacrow/server/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+wget https://github.com/datacrow/server/releases/download/${release_server}/datacrow-server.jar -O ./lib/datacrow-server.jar
